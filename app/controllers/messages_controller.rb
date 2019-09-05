64dace9165e7a51efb9382c961076265bc5bdca2
morikawa <morikawa12@gmail.com>
2019-09-05 21:33:18 +0900

    add_message_function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,28 @@
 class MessagesController < ApplicationController
+  before_action :set_group
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
-    @message = message.create(content: message_params[:content],image:message_params[:image],group_id: message_params[:group_id], user_id: current_user.id)
-    respond_to do |format|
-      format.html { redierct_to :root }
-    #   format.json { render json: @comments}
-      format.json
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
     end
   end
-end
+  
+  private
 
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+  
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+end
