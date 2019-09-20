7f8f1678dea2abbf21c7b14cda0765847bb0bd2f
morikawa <morikawa12@gmail.com>
2019-09-20 21:15:36 +0900

    add_hidouki_function0920_1

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -17,23 +17,8 @@ $(function(){
     </p>
     </div>
     <img src=${message.image} >
-  // ${image}
   </div>`
-    return html;
  } else {
-    var html =
-    `<div class="maim-bodyin" data-message-id= "${message.id}">
-    <div class="main-bodyin__box">
-      <div class="main-bodyin__box-name">
-        ${message.user.name}
-    </div>
-  <div class="main-bodyin__box-time">
- ${message.date}
-</div>
-</div>
-<div class="main-bodyin__message">
-<p class="lower-message__content">
- ${message.content}
  </p>
  </div>  
  </div> `
@@ -42,7 +27,6 @@ $(function(){
 }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
-    // console.log(this)
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
