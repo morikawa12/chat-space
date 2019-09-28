974bc5098a2e42de8c827499354318a2855bb1b8
morikawa <morikawa12@gmail.com>
2019-09-28 20:43:27 +0900

    add_incremental_search_function3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where("id NOT IN (#{current_user.id})")
+    respond_to do |format|
+    format.json
+    end   
+  end
 
   def edit
   end
